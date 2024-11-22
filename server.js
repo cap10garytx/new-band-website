92e290fedaca756ddee661b3d1ed12c497a20f6a
cap10garytx <hitchhikerstx@gmail.com>
2024-11-22 17:34:27 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,30 +2,31 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the public directory
-app.use(express.static('public'));
+// Serve static files from the root directory
+app.use(express.static(path.join(__dirname, '../../../')));
 
-// Serve the main page
+// Serve static files from the server/public directory
+app.use('/public', express.static(path.join(__dirname, '../')));
+
+// HTML routes
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, '../../../index.html'));
 });
 
-// Serve the music page
 app.get('/music', (req, res) => {
-  res.sendFile(path.join(__dirname, 'music/index.html'));
+  res.sendFile(path.join(__dirname, '../../../server/music/index.html'));
 });
 
-// Add routes for other pages
 app.get('/about', (req, res) => {
-  res.sendFile(path.join(__dirname, 'about/index.html'));
+  res.sendFile(path.join(__dirname, '../../../server/about/index.html'));
 });
 
 app.get('/shows', (req, res) => {
-  res.sendFile(path.join(__dirname, 'shows/index.html'));
+  res.sendFile(path.join(__dirname, '../../../server/shows/index.html'));
 });
 
 app.get('/links', (req, res) => {
-  res.sendFile(path.join(__dirname, 'links/index.html'));
+  res.sendFile(path.join(__dirname, '../../../server/links/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
