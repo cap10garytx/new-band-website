e9f434d290e9406df568c14dd2e34cf634c977b6
cap10garytx <hitchhikerstx@gmail.com>
2024-11-22 17:48:22 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Serve static files from the public directory
+// Log all requests to help with debugging
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+});
+
+// Serve static files from the server/public directory
 app.use(express.static(path.join(__dirname, 'server/public')));
 
-// Serve HTML files
+// HTML routes
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'server/index.html'));
 });
@@ -20,13 +26,4 @@ app.get('/about', (req, res) => {
 
 app.get('/shows', (req, res) => {
   res.sendFile(path.join(__dirname, 'server/shows/index.html'));
-});
-
-app.get('/links', (req, res) => {
-  res.sendFile(path.join(__dirname, 'server/links/index.html'));
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
 });
